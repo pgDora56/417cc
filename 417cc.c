a3e10cf671c4ace094f5fbe1d7c0d39937616e3a
Kazuki F <doradora.prog@gmail.com>
2021-06-24 08:20:08 +0900

    Add unary minus & plus

--- 417cc.c
+++ 417cc.c
@@ -5,12 +5,107 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define STR(var) #var
+
 //  Genre of token
 typedef enum {
     TK_RESERVED,
     TK_NUM,
     TK_EOF,
 } TokenKind;
+
+
+//  Genre of AST node
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // /
+    ND_NUM,  // 整数
+} NodeKind;
+
+
+typedef struct Node Node;
+
+
+//  Type of AST node
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *expr();
+Node *mul();
+Node *unary();
+Node *primary();
+void expect(char op);
+bool consume(char op);
+int expect_number();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr() {
+    Node *node = mul();
+    
+    for(;;){
+        if(consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = unary();
+
+    for (;;) {
+        if (consume('*')){
+            node = new_node(ND_MUL, node, unary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, unary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    }
+    if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
 
 typedef struct Token Token;
 
@@ -67,7 +162,7 @@ void expect(char op) {
 
 int expect_number() {
     if (token->kind != TK_NUM)
-        error_at(token->str, "数ではありません");
+        error_at(token->str, "Expected number");
     int val = token->val;
     token = token->next;
     return val;
@@ -96,7 +191,7 @@ Token *tokenize(char *p){
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -111,8 +206,41 @@ Token *tokenize(char *p){
     }
 
     new_token(TK_EOF, cur, p);
+
     return head.next;
 }
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 
 int main(int argc, char **argv) {
     if(argc != 2) {
@@ -122,25 +250,29 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize(argv[1]);
+
+    Node *node = expr();
     /* char *p = argv[1]; */
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
+    gen(node);
 
-    printf("  mov rax, %d\n", expect_number());
+    /* printf("  mov rax, %d\n", expect_number()); */
+    /*  */
+    /* while (!at_eof()) { */
+    /*     if (consume('+')) { */
+    /*         printf("  add rax, %d\n", expect_number()); */
+    /*         continue; */
+    /*     } */
+    /*  */
+    /*     expect('-'); */
+    /*     printf("  sub rax, %d\n", expect_number()); */
+    /* } */
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -20,5 +20,7 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 13 "10 - 2 + 5"
+assert 101 "10 + 20*5 - 9"
+assert 10 "-10+20"
 
 echo OK
