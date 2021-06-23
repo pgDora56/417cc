b33641d883f967573e551514851aba029536782f
Kazuki F <doradora.prog@gmail.com>
2021-06-24 04:02:05 +0900

    Improve error messages

--- 417cc.c
+++ 417cc.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Genre of token
+//  Genre of token
 typedef enum {
     TK_RESERVED,
     TK_NUM,
@@ -14,22 +14,40 @@ typedef enum {
 
 typedef struct Token Token;
 
-// Token type
+//  Token type
 struct Token {
-    TokenKind kind;  // Type of Token
-    Token *next;  // Next token
-    int val;  // value when kind is TK_NUM
-    char *str;  // token string
+    TokenKind kind;  //  Type of Token
+    Token *next;  //  Next token
+    int val;  //  value when kind is TK_NUM
+    char *str;  //  token string
 };
 
-// Watching token
+//  Watching token
 Token *token;
 
-// Alert error
+
+//  Input program
+char *user_input;
+
+//  Alert error
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+//  Report error
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");  // Output pos blank
+    fprintf(stderr, "^ ");
+    fprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
@@ -43,13 +61,13 @@ bool consume(char op) {
 
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -89,7 +107,7 @@ Token *tokenize(char *p){
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "expected number");
     }
 
     new_token(TK_EOF, cur, p);
@@ -102,6 +120,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    user_input = argv[1];
     token = tokenize(argv[1]);
     /* char *p = argv[1]; */
 
